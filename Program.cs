ab0f52fc7b5ff99df42a26b96c9c4b850fbd7c1c
aogum <aogum@GUMENNIKOVAO>
2023-11-08 11:35:57 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace In_Line_Drive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
