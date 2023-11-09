cd7e563033010aa945ef4d3b9b59fc4600146c14
aogum <aogum@GUMENNIKOVAO>
2023-11-09 12:24:51 +0500

    new

--- Program.cs
+++ Program.cs
@@ -13,30 +13,49 @@ namespace In_Line_Drive
         static void Main(string[] args)
         {
             
-            try
-            {
+            
                 //создание объекта для отправки данных на табло(в конструктор передается порт для открытия сокета)
                 int numberport = 0;
                 Console.WriteLine("Введите номер порта");
                 numberport=Console.Read();
                 
                 ScoreboardSender sender = new ScoreboardSender(numberport);
+            try
+            {
                 //поиск табло возвращает словарь с табло(key - MAC)
                 var scoreboards = sender.FindScoreboards();
                 foreach (var scoreboard in scoreboards)
                 {
                     Console.WriteLine($"Найдено табло:{scoreboard.Value}");
                 }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Поиск табло неудачно"+ex);
+            }
+            try
+            {
                 int brigthness = 100;
                 //пример установки яркости табло(практически везде можно передать target для установки, это mac).
                 //Если передать null запрос будет широковещательным на всю сеть
                 sender.SetBrightness(brigthness, null);
-
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Яркость неудачно" + ex);
+            }
+            try
+            {
                 //пример установки ячейки RSTR(передается индекс ячейки в табло, выравнивание строки и сама строка) + target
                 sender.SetNvstrData(new StringMessage(0, StringProfile.StrCenter, "ИЖ0"), null);
 
-
-
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Nvstr неудачно" + ex);
+            }
+            try 
+            { 
                 List<StringMessage> stringMessages = new List<StringMessage>();
                 stringMessages.Add(new StringMessage(0, StringProfile.StrLeft, "ПП"));
 
@@ -45,10 +64,11 @@ namespace In_Line_Drive
                 sender.SetRstrData(stringMessages, null);
 
                 
+                    
             }
             catch(Exception ex)
             {
-                Console.WriteLine(ex);
+                Console.WriteLine("Rstr неудачно" + ex);
             }
         }
         
